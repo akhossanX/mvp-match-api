1c2da5e9267a426cda970a900ff8859af3bc87d8
Abdelilah <abdelilah.khossan@wayo.cloud>
2021-10-03 09:09:35 +0100

    Add user create view

--- vending_machine/views.py
+++ vending_machine/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+
+from rest_framework.generics import GenericAPIView
+from rest_framework.response import Response
+from rest_framework import status
+from .serializer import *
+
 
 # Create your views here.
+
+class UserCreateAPIView(GenericAPIView):
+    serializer_class = UserCreateSerializer
+
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(data=serializer.data, status=status.HTTP_201_CREATED)
+
+        return Response(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
