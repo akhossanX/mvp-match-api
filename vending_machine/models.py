7c90be33ace56967a60f3a743d2bc612290a8d2c
Abdelilah <abdelilah.khossan@wayo.cloud>
2021-10-03 09:07:38 +0100

    Alter custom user model

--- vending_machine/models.py
+++ vending_machine/models.py
@@ -1,15 +1,35 @@
 from django.db import models
 
-from django.db.models.deletion import CASCADE
-from django.contrib.auth.models import User as DjangoUser
-
-
 # Create your models here.
 
 
 class User(models.Model):
-    user = models.OneToOneField(DjangoUser, on_delete=models.CASCADE, related_name='user_profile')
-    role = models.CharField(max_length=255)
+
+    class DepositChoices(models.IntegerChoices):
+        COIN_ZERO = 0  # Default deposit upon user creation or if no deposit is supplied
+        COIN_FIVE = 5
+        COIN_TEN = 10
+        COIN_TWENTY = 20
+        COIN_FIFTY = 50
+        COIN_HUNDRED = 100
+
+    class UserRoleChoices(models.TextChoices):
+        SELLER = 'seller'
+        BUYER = 'buyer'
+
+    username = models.CharField(max_length=255, unique=True, blank=False)
+    password = models.CharField(max_length=255, blank=False)
+    deposit = models.IntegerField(
+        choices=DepositChoices.choices,
+        default=0,
+        null=False
+    )
+    role = models.CharField(
+        max_length=255,
+        choices=UserRoleChoices.choices,
+        default='buyer',
+        null=False
+    )
 
 
 class Product(models.Model):
