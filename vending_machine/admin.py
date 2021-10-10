3989ad63934936ed637c80fb956a38196e97e2a4
Abdelilah <abdelilah.khossan@wayo.cloud>
2021-10-10 22:51:23 +0100

    register User model in admin

--- vending_machine/admin.py
+++ vending_machine/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
+from vending_machine.models import User
+from django.contrib.auth.admin import UserAdmin
+
 
 # Register your models here.
+
+admin.register(User, UserAdmin)
 
